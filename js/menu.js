9f6887b903deee5eafe54049f164bb1c52289322
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-11-26 00:26:47 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -32,19 +32,17 @@ function mostrar(){
 
             });
             storageRef.put(file).on('state_changed', (snapshot) => {
-
+              const storageRef = firebase.storage().ref('images/' + file.name);
+              storageRef.getDownloadURL().then(function(url){
+                    
+                const image = document.getElementById('image');
+                console.log(url);
+                image.src = url
+                      a= url;
+            });
               
             });
             
         });
 
-        function traerimagen(){
-          const storageRef = firebase.storage().ref('images/' + file.name);
-          storageRef.getDownloadURL().then(function(url){
-                
-            const image = document.getElementById('image');
-            console.log(url);
-            image.src = url
-                  a= url;
-        });
-        } 
+        
