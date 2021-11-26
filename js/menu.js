a7b709b502feb2c2d7b0574cc7b8da40e7821195
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-11-26 00:20:27 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -14,7 +14,7 @@ var config = {
       
     var a ="";
     var l = "";
-      function mostrar(){
+function mostrar(){
           document.getElementById('progreso').innerHTML = ' <progress id="progress_bar" value="0" max="100"></progress>';
           document.getElementById('hola').innerHTML = ' <img width="100px" height="100px" src="" alt="" id="image">';}
         document.getElementById('file').addEventListener('change', (event) => {
@@ -28,15 +28,19 @@ var config = {
         
                 const progressBar = document.getElementById('progress_bar');
                 progressBar.value = progress;
-
+                traerimagen()
             });
             
-            storageRef.getDownloadURL().then(function(url){
-                
-                const image = document.getElementById('image');
-                console.log(url);
-                image.src = url
-                      a= url;
-            });
+            
         });
-  
+
+        function traerimagen(){
+          const storageRef = firebase.storage().ref('images/' + file.name);
+          storageRef.getDownloadURL().then(function(url){
+                
+            const image = document.getElementById('image');
+            console.log(url);
+            image.src = url
+                  a= url;
+        });
+        } 
