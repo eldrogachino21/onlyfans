fe83affb66e203bb2daa08904609ef3cbc5c8cf2
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-11-26 02:52:11 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -24,6 +24,7 @@ document.getElementById("content").innerHTML=""
 }
 
 function inicio(){
+  document.getElementById("content").innerHTML=""
   document.getElementById("content").innerHTML+='div class="row" id="jarabeDiv"></div>      <h2 id="pastillas">Publicaciones</h2>      <hr>      <div class="row" id="pastillasDiv"></div>      <h2 style="color: rgb(226, 241, 255);" id="comprimidos">Capsulas</h2>      <hr>      <div class="row" id="comprimidosDiv"></div>      <h2 style="color: rgb(226, 241, 255);" id="polvos">Polvos</h2>      <hr>      <div class="row" id="polvosDiv"></div>      <h2 style="color: rgb(226, 241, 255);" id="polvos"></h2>      <hr class="featurette-divider"></hr>'
 }
 
