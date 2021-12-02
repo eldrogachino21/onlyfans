990c2e5ca1148367c2fa74218aafa939d07a034c
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-02 05:09:59 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -279,7 +279,7 @@ function cards(){
         
 
         if(taskV.categoria=="costo"){
-        if(tokens[0].tokens==""){
+        
         
         let URL = `${taskV.imagen}`;
         let btn = `btnjarabe${con}`;
@@ -307,7 +307,7 @@ function cards(){
              </div> 
         </div>
          </div>`;
-        }else if(tokens[0].tokens==taskV.id){
+       if(tokens[0].tokens==taskV.id){
           document.getElementById('jarabeDiv').innerHTML += `
           <div id="data${taskV.id}" class="bg-white max-w-sm mx-auto rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl "><div class="card"><div class="card mb-4 shadow-sm">
                  <img onclick="mostrar(','','','${URL}','${con}','${btn}')" class="card-img-top" style=" height:28rem; width:23rem;" src="${URL}"
