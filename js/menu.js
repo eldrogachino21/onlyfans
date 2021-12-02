768861588757eecc1cf7a8c293e946e94441d149
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-02 02:29:23 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,4 @@
-var config = {
-    apiKey: "AIzaSyC1KT3aBbQbx_LTpcdSO995B_f0blI8fzw",
-    authDomain: "onlyfansclone-3ada1.firebaseapp.com",
-    projectId: "onlyfansclone-3ada1",
-    databaseURL: "https://onlyfansclone-3ada1-default-rtdb.firebaseio.com",
-    storageBucket: "onlyfansclone-3ada1.appspot.com",
-    messagingSenderId: "1010568675424",
-    appId: "1:1010568675424:web:9dd1d1eb8d5922287ac305",
-    measurementId: "G-2P0E5VP2LV"
-  };
-  firebase.initializeApp(config);
+
 
   
       
