261f02c905c5c5f0e23d7314849c63a5f13852b4
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-02 04:45:29 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -279,7 +279,7 @@ function cards(){
        <h6   align="center" class="card-subtitle mb-2 text-muted">${taskV.descripcion} </h6>
        <h5 style="display:block;>${taskV.descripcion}</h5>
        <div align="center" class="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
-       <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-20 justify-center items-center" href="pagar.html">${taskV.precio}</span>
+       <span align="center" class="inline-flex bg-pink-600 text-white rounded-full h-6 px-20 justify-center items-center" href="pagar.html">${taskV.precio}</span>
      </div>
 
        </div>
@@ -325,7 +325,7 @@ function cards(){
               <div class="-m-2 text-center">
   <div class="p-2">
     <div align="center" class="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
-      <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-20 justify-center items-center" href="pagar.html">${taskV.precio}</span>
+      <span align="center" class="inline-flex bg-pink-600 text-white rounded-full h-6 px-20 justify-center items-center" href="pagar.html">${taskV.precio}</span>
     </div>
   </div>
               <h5 style="display:block;>${taskV.descripcion}</h5>
