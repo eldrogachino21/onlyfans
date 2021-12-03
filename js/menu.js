86ec20a3ef4d40f71a69de7187b2a5bc9b793a28
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-03 06:43:03 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -37,7 +37,7 @@ function perfil(){
   <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
       <div class="text-center">
           <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">Perfil  <span class="text-blue-400 underline">Saas</span></h1>
-          <input id="file2" type="fie" class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</input>
+          <input type="file" id="file" class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500" required>
       </div>
   </div>
 </div>
