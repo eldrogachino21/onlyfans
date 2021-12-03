e267a616215c6468561713d1b3b523c54a956e4a
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-03 03:16:45 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -124,7 +124,7 @@ alert("publicado con exito");
         
 function actualizar(){
   let persona = JSON.parse(localStorage.getItem("datos"));
-  var task = firebase.database().ref("pagados/"+persona[0].telefono+);
+  var task = firebase.database().ref("pagados/"+persona[0].telefono);
     
   task.on("child_added", function(data) {
       
