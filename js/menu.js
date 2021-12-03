140cd02b4766ccb62d709e8f1230bd6636047d4c
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2021-12-03 06:41:58 -0600

    s

--- js/menu.js
+++ js/menu.js
@@ -37,11 +37,10 @@ function perfil(){
   <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
       <div class="text-center">
           <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">Perfil  <span class="text-blue-400 underline">Saas</span></h1>
-          <button id="file2" type="fie" class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
+          <input id="file2" type="fie" class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</input>
       </div>
   </div>
 </div>
-<img class="max-h-20 w-full opacity-80 absolute top-0" style="z-index:-1" src="https://unsplash.com/photos/iFPBRwZ4I-M/download?force=true&w=640" alt="" />
 
 `;
 }
