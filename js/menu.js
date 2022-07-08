5fe74da8c8683ac3e4972d65bfb1649dd4214e19
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-07-08 14:02:49 -0500

    xs

--- js/menu.js
+++ js/menu.js
@@ -239,7 +239,7 @@ var persona = JSON.parse(localStorage.getItem("datos"));
 
 function pagados(con){
 
-  var pagado = firebase.database().ref("pagados/"+persona[0].telefono+"/"+con);
+  var pagado = firebase.database().ref("pagados/");
     
     pagado.on("child_added", function(data) {
         
