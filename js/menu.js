c857e786b63603176ca7756d3417704d5c42e533
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-09-03 13:04:40 -0500

    s

--- js/menu.js
+++ js/menu.js
@@ -24,8 +24,9 @@ function pagar(contador,tokens,pagado){
       pagado:"pagado"
   
   }
-alert("pagado con exito");
   db.set(itemdb);
+  location.replace('menu2.html');
+
   }
 
 function perfil2(){
